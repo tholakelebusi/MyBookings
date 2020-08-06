2bb3934104ca4bdfcd03ca904b16ff68dd8ae926
tholakelebusi <tholakelebusi@gmail.com>
2020-08-06 01:24:05 +0000

    2 files modified

--- index.js
+++ index.js
@@ -28,26 +28,67 @@ appDiv.innerHTML = `<title>Booking Flight</title>
 <div class="hhh">TRAVEL SAFE WITH US</div>
 </center>
 
-<center>
-    <div class="time">
-        <label>Booking Date</label>
-<input type="datetime-local">
-</div>
-</center>
+
+
 
 <center>
-<div id="button">
-<button>Booking</button>
-<button>Staying</button>
-<button>Few Days</button>
-<button>Return</button>
+
+<div class="button">
+
+
+<button id="bu">FROM</button>
+<button id="bu">TO</button>
+<button id="bu">Date</button>
+<button id="bu">Search Flights</button>
+
+
 </div>
+
 </center>
 
-</style>
+<div class="pictures">
+
+
+   <a><img src="https://i.ytimg.com/vi/Lx_YQ7c674o/maxresdefault.jpg
+" width="150cm" height="210px"hjk ></a>
+   <a><img src="https://www.hindustantimes.com/rf/image_size_960x540/HT/p2/2019/07/24/Pictures/_85208268-ad9a-11e9-a723-55bad35bb354.png" width="150cm" height="210px" ></a>
+   <a><img src="https://www.fabhotels.com/blog/wp-content/uploads/2018/04/DF.jpg" width="150cm" height="210px" ></a>
+   <a><img src="https://i.ytimg.com/vi/UJ6GmaZVFkU/maxresdefault.jpg" width="150cm" height="210px" ></a>
+  
+</div>
+
+<div class="btn">
+<h2>Extra information</h2>
+<p>-view specials</p>
+</div>
+
+<div class="btn1">
+<h3>Guest</h3>
+<p>*Add more guest</p>
+<p>*under 18</p>
+</div>
+
+<div class="btn2">
+<h4>Hotel</h4>
+<td style="width:427px;">
+<p>#Available discounts</p>
+<p>#Terms and conditions</p>
+</td>
+</div>
+
 <center>
 <div class="comment">
-comment: <textarea name="comment" rows="4" cols="30">
+comment: <textarea name="comments" rows="4" cols="30">
 </textarea>
 </div>
-</center>`;
+</center>
+
+
+
+<div class="buttons">
+<button>Submit your comment</button>
+</div>
+
+<div id="bb">
+<button type="button" id="bb">Thanks for visitng our website came back again</button>
+</div>`;
